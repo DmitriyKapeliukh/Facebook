756b4c90a4b01c8b7d2fb708f617bd1122023d2a
Dmytro_Kapeliukh <welcome.t.t100@gmail.com>
2017-04-11 18:36:57 +0300

    dasd

--- src/test/java/com/facebook/tests/SignUpTest.java
+++ src/test/java/com/facebook/tests/SignUpTest.java
@@ -2,6 +2,11 @@ package com.facebook.tests;
 
 import com.qaautomation.pages.FacebookMainPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 /**
  * Created by Dmytro_Kapeliukh on 4/11/2017.
@@ -9,4 +14,20 @@ import org.openqa.selenium.WebDriver;
 public class SignUpTest {
     public WebDriver driver;
     FacebookMainPage fbMainPage;
+
+    @BeforeClass(alwaysRun = true)
+    public void setup(){
+        this.driver = new FirefoxDriver();
+        fbMainPage = PageFactory.initElements(driver, FacebookMainPage.class);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void teardown(){
+        this.driver.quit();
+    }
+
+    @Test(groups = "p1")
+    public void testSignUpMainPage(){
+
+    }
 }
